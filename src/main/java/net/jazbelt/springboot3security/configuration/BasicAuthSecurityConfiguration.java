ae1bb3b71232084fbce6e23ea5d51a0d2a55d221
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-23 17:28:05 +0100

    JWT setup

---
+++ src/main/java/net/jazbelt/springboot3security/configuration/BasicAuthSecurityConfiguration.java
@@ -0,0 +1,89 @@
+package net.jazbelt.springboot3security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+// @Configuration
+public class BasicAuthSecurityConfiguration {
+
+    @SuppressWarnings("removal")
+    @Bean
+    SecurityFilterChain secFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests( auth -> {
+            auth.anyRequest().authenticated();
+        });
+        http.sessionManagement(session -> {
+            session.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        });
+        http.httpBasic();
+
+        http.csrf().disable();
+
+        http.headers().frameOptions().sameOrigin();
+
+        return http.build();
+    }
+
+    /*@Bean
+    public UserDetailsService userDetailService() {
+        UserDetails user = User.withUsername("fakeuser")
+                .password("{noop}dummypass")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("fakeadmin")
+                .password("{noop}dummypass")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
+    }*/
+
+    @Bean
+    public UserDetailsService userDetailService(DataSource dataSource) {
+        UserDetails user = User.withUsername("fakeuser")
+                //.password("{noop}dummypass")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("fakeadmin")
+                //.password("{noop}dummypass")
+                .password("dummy")
+                .passwordEncoder(str -> passwordEncoder().encode(str))
+                .roles("ADMIN", "USER")
+                .build();
+
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(dataSource);
+        manager.createUser(user);
+        manager.createUser(admin);
+
+        return manager;
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/net/jazbelt/springboot3security/configuration/JWTSecurityConfiguration.java
+++ src/main/java/net/jazbelt/springboot3security/configuration/JWTSecurityConfiguration.java
@@ -1,24 +1,38 @@
-package net.jazbelt.springboot3security;
+package net.jazbelt.springboot3security.configuration;
 
+import com.nimbusds.jose.KeySourceException;
+import com.nimbusds.jose.jwk.JWK;
+import com.nimbusds.jose.jwk.JWKSelector;
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+import com.nimbusds.jose.jwk.source.JWKSource;
+import com.nimbusds.jose.proc.SecurityContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import javax.sql.DataSource;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.interfaces.RSAPublicKey;
+import java.util.List;
+import java.util.UUID;
 
 @Configuration
-public class BasicAuthSecurityConfiguration {
+public class JWTSecurityConfiguration {
 
     @SuppressWarnings("removal")
     @Bean
@@ -35,23 +49,10 @@ public class BasicAuthSecurityConfiguration {
 
         http.headers().frameOptions().sameOrigin();
 
+        http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+
         return http.build();
     }
-
-    /*@Bean
-    public UserDetailsService userDetailService() {
-        UserDetails user = User.withUsername("fakeuser")
-                .password("{noop}dummypass")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.withUsername("fakeadmin")
-                .password("{noop}dummypass")
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }*/
 
     @Bean
     public UserDetailsService userDetailService(DataSource dataSource) {
@@ -87,5 +88,33 @@ public class BasicAuthSecurityConfiguration {
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public KeyPair keyPair() throws Exception {
+        var kpGenerator = KeyPairGenerator.getInstance("RSA");
+        kpGenerator.initialize(4096);
+        return kpGenerator.generateKeyPair();
+    }
+
+    @Bean
+    public RSAKey rsaKey(KeyPair keyPair) {
+        return new RSAKey.Builder((RSAPublicKey) keyPair.getPublic())
+                .privateKey(keyPair.getPrivate())
+                .keyID(UUID.randomUUID().toString())
+                .build();
+    }
+
+    @Bean
+    public JWKSource<SecurityContext> jwkSource(RSAKey rsaKey) {
+        var jwkSet = new JWKSet(rsaKey);
+
+        return (jwkSelector, securityContext) -> jwkSelector.select(jwkSet);
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder(RSAKey rsaKey) throws Exception {
+        return NimbusJwtDecoder.withPublicKey(rsaKey.toRSAPublicKey())
+                .build();
     }
 }
