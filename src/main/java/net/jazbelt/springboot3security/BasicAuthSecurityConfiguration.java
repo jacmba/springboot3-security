c199f1c47b51d5f9a7cb96d4bc072aae98dcca71
Jacinto Mba Cantero <jacmba@gmail.com>
2024-04-23 13:28:15 +0100

    In memory users

--- src/main/java/net/jazbelt/springboot3security/BasicAuthSecurityConfiguration.java
+++ src/main/java/net/jazbelt/springboot3security/BasicAuthSecurityConfiguration.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -23,5 +27,20 @@ public class BasicAuthSecurityConfiguration {
         http.csrf().disable();
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailService() {
+        UserDetails user = User.withUsername("fakeuser")
+                .password("{noop}dummypass")
+                .roles("USER")
+                .build();
+
+        UserDetails admin = User.withUsername("fakeadmin")
+                .password("{noop}dummypass")
+                .roles("ADMIN")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 }
